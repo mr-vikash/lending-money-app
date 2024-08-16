fc56ace38f0ecd3defb54b408b758e23f206381a
mr-vikash <mrvikash700@gmail.com>
2024-08-16 17:37:14 +0530

    adding loans controller inside admin

---
+++ app/controllers/admin/loans_controller.rb
@@ -0,0 +1,30 @@
+class Admin::LoansController < ApplicationController
+  def update
+    @loan = Loan.find(params[:id])
+    case params[:commit]
+    when 'Approve'
+      approve_loan
+    when 'Reject'
+      reject_loan
+    when 'Adjust'
+      adjust_loan
+    end
+  end
+
+  private
+
+  def approve_loan
+    @loan.update(status: 'approved')
+  end
+
+  def reject_loan
+    @loan.update(status: 'rejected')
+  end
+
+  def adjust_loan
+    @loan.update(status: 'waiting_for_adjustment_acceptance', amount: params[:loan][:amount], interest_rate: params[:loan][:interest_rate])
+    LoanAdjustment.create(loan: @loan, adjusted_amount: params[:loan][:amount], adjusted_interest_rate: params[:loan][:interest_rate])
+  end
+end
+
+
