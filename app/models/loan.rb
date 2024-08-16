f185b6a2fccc05012f658ceb8f74c0cff8fb303a
mr-vikash <mrvikash700@gmail.com>
2024-08-16 18:04:07 +0530

    implemented logic for adjusted intrest rate

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -4,4 +4,10 @@ class Loan < ApplicationRecord
   has_many :loan_adjustments
   validates :amount, :interest_rate, numericality: { greater_than: 0 }
   validates :status, inclusion: { in: %w[requested approved open closed rejected waiting_for_adjustment_acceptance readjustment_requested] }
+  
+  def adjustment_history
+    loan_adjustments.map do |adj|
+      "Adjusted Amount: #{adj.adjusted_amount}, Adjusted Interest Rate: #{adj.adjusted_interest_rate}"
+    end
+  end
 end
