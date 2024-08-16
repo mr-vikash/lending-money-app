2519563d80e7fe9f6d5066960d7cf103b0f603e9
mr-vikash <mrvikash700@gmail.com>
2024-08-16 17:41:12 +0530

    Implement user actions for accepting, rejecting, or requesting a readjustment of the loan.

--- app/controllers/user/loans_controller.rb
+++ app/controllers/user/loans_controller.rb
@@ -12,7 +12,37 @@ class User::LoansController < ApplicationController
     end
   end
 
+  def update
+    @loan = Loan.find(params[:id])
+    case params[:commit]
+    when 'Accept'
+      accept_adjustment
+    when 'Reject'
+      reject_loan
+    when 'Request Readjustment'
+      request_readjustment
+    end
+  end
+
   private
+  
+  def accept_adjustment
+    @loan.update(status: 'open')
+    update_wallets
+  end
+
+  def reject_loan
+    @loan.update(status: 'rejected')
+  end
+
+  def request_readjustment
+    @loan.update(status: 'readjustment_requested')
+  end
+
+  def update_wallets
+    @loan.user.update(wallet_balance: @loan.user.wallet_balance + @loan.amount)
+    @loan.admin.update(wallet_balance: @loan.admin.wallet_balance - @loan.amount)
+  end
 
   def loan_params
     params.require(:loan).permit(:amount, :interest_rate)
